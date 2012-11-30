fa7b724206694a4ac991f8958e899f00451e00fb
Bastien Dejean <nihilhill@gmail.com>
2012-11-30 14:51:53 +0100

    Update

---
+++ js/mailto.js
@@ -0,0 +1,20 @@
+function insert_mailto() {
+    var link = document.getElementById('contact');
+    var url = 'gpngabP02%Q5%ryyvierxfnoO5%=gprwohf?zbp.gvoniny@ryyvierxfno:bgyvnz';
+
+    if (link != null)
+        link.href = url.split('').reverse().map(rot_thirteen).join('');
+}
+
+function rot_thirteen(c) {
+    if (!c.match(/[A-Za-z]/)) {
+        return c;
+    } else {
+        var code = c.charCodeAt(0);
+        var minval = 97;
+        if (code < minval)
+            minval = 65;
+        code = minval + ((code - minval) + 13) % 26;
+        return String.fromCharCode(code);
+    }
+}
